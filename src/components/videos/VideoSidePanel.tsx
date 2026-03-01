fc583a741f24bd86ae831acb6d2ff325bb0782bf
nadlex <aleksejzenkov2003@gamil.com>
2026-03-01 18:41:57 +0000

    Changes

--- src/components/videos/VideoSidePanel.tsx
+++ src/components/videos/VideoSidePanel.tsx
@@ -270,7 +270,7 @@ export function VideoSidePanel({
 
   return (
     <Sheet open={open} onOpenChange={onOpenChange}>
-      <SheetContent className="w-[450px] sm:w-[550px] p-0 flex flex-col">
+      <SheetContent className="w-[450px] sm:w-[550px] p-0 flex flex-col [&>button.absolute]:hidden">
         {/* Header */}
         <div className="flex items-center justify-between px-4 py-3 border-b">
           <div className="flex items-center gap-2">
